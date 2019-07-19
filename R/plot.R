31dedbb172b48ae6cc5f78c3f2dfe2cf82cbf831
Aaron Conway <aaronconwayphd@gmail.com>
2019-07-19 14:56:40 -0400

    data and plot

---
+++ R/plot.R
@@ -0,0 +1,23 @@
+#' Plot temperatures
+#'
+#' @return Line plot
+#' @param x x axis
+#' @param y y axis
+#' 
+#' @name plot
+#' @rdname plot
+#' 
+#' @examples 
+#' plot(x=time, y=temp)
+#' 
+#' 
+#' @export
+plot <- function(x,y){
+ggplot2::ggplot(data = temps, aes(x = x, y = y, group = ID)) + 
+  geom_line(aes(colour=ID), show.legend=F) + 
+  labs(x="Measurement", y="Temperature") + theme_bw() + 
+  theme(panel.border = element_blank(),  
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"))
+}
